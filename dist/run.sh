14c26b38558f570f04e8e0e82364d93e744087cd
Erlend Mongstad <eeerlend@gmail.com>
2019-05-14 13:42:25 +0200

    fix: Skips setting config if already set

--- dist/run.sh
+++ dist/run.sh
@@ -6,6 +6,12 @@ for ((i=0; i<${#macup_scutil_prefs[@]}; ++i)); do
   pref="$(echo "${macup_scutil_prefs[i]}" | cut -d':' -f1)"
   value="$(echo "${macup_scutil_prefs[i]}" | cut -d':' -f2)"
 
-  report_from_package "Setting \"$pref\" to \"$value\" "
-  sudo scutil --set "$pref" "$value"
+  current_value="$(scutil --get $pref)"
+
+  if [ "$current_value" != "$value" ]; then
+    report_from_package "Setting \"$pref\" to \"$value\" "
+    sudo scutil --set "$pref" "$value"
+  else
+    report_from_package "\"$pref\" is already set to \"$current_value\" "
+  fi
 done
